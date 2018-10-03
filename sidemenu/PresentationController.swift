83795ab5a949cfca2b025c2d4eabadef196f7a20
Arun Saxena <admin@admins-macbook-pro.local>
2018-10-03 15:27:02 +0530

    Removed console logs

---
+++ sidemenu/PresentationController.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationController.swift
+//  sidemenu
+//
+//  Created by Admin on 03/10/18.
+//  Copyright © 2018 Equitronix. All rights reserved.
+//
+
+import UIKit
+
+class PresentationController: UIPresentationController {
+
+    override var shouldRemovePresentersView: Bool {
+        return false;
+    }
+
+}
